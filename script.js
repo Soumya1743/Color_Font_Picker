3a744f4d86cfca67d90b3b43e73350f9d53138e1
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-09-22 21:33:27 +0530

    Color_Font_Picker

---
+++ script.js
@@ -0,0 +1,250 @@
+let type_here = document.getElementById('type_here');
+let bgcolors = document.getElementsByClassName('bgcolors');
+let colors = document.getElementsByClassName('colors');
+let style = document.getElementsByClassName('font_style');
+
+bgcolors[0].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "red";
+    bgcolors[0].style.border = "1px solid black";
+});
+bgcolors[1].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "green";
+});
+bgcolors[2].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "rgb(255, 0, 149)";
+});
+bgcolors[3].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "mediumvioletred";
+});
+bgcolors[4].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "blue";
+});
+bgcolors[5].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "skyblue";
+});
+bgcolors[6].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "salmon";
+});
+bgcolors[7].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "violet";
+});
+bgcolors[8].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "yellow";
+});
+bgcolors[9].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "yellowgreen";
+});
+bgcolors[10].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "royalblue";
+});
+bgcolors[11].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "tomato";
+});
+bgcolors[12].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "teal";
+});
+bgcolors[13].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "turquoise";
+});
+bgcolors[14].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "indigo";
+});
+bgcolors[15].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "orange";
+});
+bgcolors[16].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "slategrey";
+});
+bgcolors[17].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "darkcyan";
+});
+bgcolors[18].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "darkblue";
+});
+bgcolors[19].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "firebrick";
+});
+bgcolors[20].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "yellowgreen";
+});
+bgcolors[21].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "hotpink";
+});
+bgcolors[22].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "blueviolet";
+});
+bgcolors[23].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "navy";
+});
+bgcolors[24].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "mediumturquoise";
+});
+bgcolors[25].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "red";
+});
+bgcolors[26].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "rgb(255, 85, 85)";
+});
+bgcolors[27].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "rgb(255, 180, 180)";
+});
+bgcolors[28].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "rgb(255, 159, 225)";
+});
+bgcolors[29].addEventListener('click', () =>{
+    type_here.style.backgroundColor = "rgb(82, 82, 82)";
+});
+
+
+colors[0].addEventListener('click', () =>{
+    type_here.style.color = "red";
+});
+colors[1].addEventListener('click', () =>{
+    type_here.style.color = "green";
+});
+colors[2].addEventListener('click', () =>{
+    type_here.style.color = "rgb(255, 0, 149)";
+});
+colors[3].addEventListener('click', () =>{
+    type_here.style.color = "mediumvioletred";
+});
+colors[4].addEventListener('click', () =>{
+    type_here.style.color = "blue";
+});
+colors[5].addEventListener('click', () =>{
+    type_here.style.color = "skyblue";
+});
+colors[6].addEventListener('click', () =>{
+    type_here.style.color = "salmon";
+});
+colors[7].addEventListener('click', () =>{
+    type_here.style.color = "violet";
+});
+colors[8].addEventListener('click', () =>{
+    type_here.style.color = "yellow";
+});
+colors[9].addEventListener('click', () =>{
+    type_here.style.color = "yellowgreen";
+});
+colors[10].addEventListener('click', () =>{
+    type_here.style.color = "royalblue";
+});
+colors[11].addEventListener('click', () =>{
+    type_here.style.color = "tomato";
+});
+colors[12].addEventListener('click', () =>{
+    type_here.style.color = "teal";
+});
+colors[13].addEventListener('click', () =>{
+    type_here.style.color = "turquoise";
+});
+colors[14].addEventListener('click', () =>{
+    type_here.style.color = "indigo";
+});
+colors[15].addEventListener('click', () =>{
+    type_here.style.color = "orange";
+});
+colors[16].addEventListener('click', () =>{
+    type_here.style.color = "slategrey";
+});
+colors[17].addEventListener('click', () =>{
+    type_here.style.color = "darkcyan";
+});
+colors[18].addEventListener('click', () =>{
+    type_here.style.color = "darkblue";
+});
+colors[19].addEventListener('click', () =>{
+    type_here.style.color = "firebrick";
+});
+colors[20].addEventListener('click', () =>{
+    type_here.style.color = "greenyellow";
+});
+colors[21].addEventListener('click', () =>{
+    type_here.style.color = "hotpink";
+});
+colors[22].addEventListener('click', () =>{
+    type_here.style.color = "blueviolet";
+});
+colors[23].addEventListener('click', () =>{
+    type_here.style.color = "navy";
+});
+colors[24].addEventListener('click', () =>{
+    type_here.style.color = "mediumturquoise";
+});
+colors[25].addEventListener('click', () =>{
+    type_here.style.color = "red";
+});
+colors[26].addEventListener('click', () =>{
+    type_here.style.color = "rgb(255, 85, 85)";
+});
+colors[27].addEventListener('click', () =>{
+    type_here.style.color = "rgb(255, 180, 180)";
+});
+colors[28].addEventListener('click', () =>{
+    type_here.style.color = "rgb(255, 159, 225)";
+});
+colors[29].addEventListener('click', () =>{
+    type_here.style.color = "rgb(82, 82, 82)";
+});
+
+
+style[0].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Times New Roman";
+});
+style[1].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Roboto";
+});
+style[2].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Courier New";
+});
+style[3].addEventListener('click', () =>{
+    type_here.style.fontFamily = "cursive";
+});
+style[4].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Verdana";
+});
+style[5].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Impact";
+});
+style[6].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Arial";
+});
+style[7].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Georgia";
+});
+style[8].addEventListener('click', () =>{
+    type_here.style.fontFamily = "serif";
+});
+style[9].addEventListener('click', () =>{
+    type_here.style.fontFamily = "sans-serif";
+});
+style[10].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Arial";
+});
+style[11].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Courier";
+});
+style[12].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Helvetica";
+});
+style[13].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Verdana";
+});
+style[14].addEventListener('click', () =>{
+    type_here.style.fontFamily = "Times";
+});
+
+
+
+
+
+let d = document.getElementById('download');
+d.addEventListener('click', download_image);
+function download_image(){
+    html2canvas(document.getElementById('text_box')).then(canvas=>{
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'image.png';
+        link.click();
+    });
+}
